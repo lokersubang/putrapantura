a50407d77941489c9d684463f760940504bbeb2d
Dadan28 <dadannurmaulana@gmail.com>
2023-02-18 21:40:16 +0700

    Menambahkan Analityc

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -6,6 +6,19 @@
     <meta name="viewport" content="width=device-width, initial-scale=1">
     {{-- <title inertia>{{ config('app.name', 'Laravel') }}</title> --}}
 
+    <!-- Google tag (gtag.js) -->
+    <script async src="https://www.googletagmanager.com/gtag/js?id=UA-185896191-1"></script>
+    <script>
+        window.dataLayer = window.dataLayer || [];
+
+        function gtag() {
+            dataLayer.push(arguments);
+        }
+        gtag('js', new Date());
+
+        gtag('config', 'UA-185896191-1');
+    </script>
+
     <!-- our project just needs Font Awesome Solid + Brands -->
     <link rel="icon" href="/img/icon.ico" />
     <link href="/fontawesome/css/fontawesome.css" rel="stylesheet">
